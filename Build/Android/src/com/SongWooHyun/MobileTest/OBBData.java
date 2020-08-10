a4f67f7f64e5d315f91909f6fe84eec811621bfc
WooHyun <lkjfrf@naver.com>
2020-08-10 12:09:25 +0900

    Change

--- Build/Android/src/com/SongWooHyun/MobileTest/OBBData.java
+++ Build/Android/src/com/SongWooHyun/MobileTest/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-122335793L // the length of the file in bytes
+124192369L // the length of the file in bytes
 )
 };
 };
